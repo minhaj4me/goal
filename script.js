d9e03774cfdc65673dcf189d7861b85cb284f683
minhaj4me <minhajhaider007@gmail.com>
2024-04-29 17:22:00 +0530

    goal_v2

--- script.js
+++ script.js
@@ -2,14 +2,61 @@
 const checkboxes = document.querySelectorAll(".custom-checkbox");
 //inputboxes
 const inputboxes = document.querySelectorAll(".goal-input");
+const quote = document.querySelector(".pBeforeBar");
+const footQuote = document.querySelector(".motivatesLine")
 //error line
 const error = document.querySelector(".pAfterBar");
 
+let barValue = document.querySelector(".barValue")
 
-const barValue = document.querySelector(".barValue");
-const numberOfTask = document.querySelector(".numberOfTask");
+const allQuotes = [
+  'Raise the bar by completing your goals!',
+  'Well begun is half done!',
+  'Just a step away keep going!',
+  'Whoa! You just completed all goals, time for chill :D'
+]
 
-var numOfT = parseInt(numberOfTask.innerHTML.slice(0, 1));
+const footQuotes = [
+  '"Move one step ahead, today!"',
+  '"Keep Going, You are making great progress!"',
+  '"Keep Going, You are making great progress!"',
+  '"Voila! You Did it"'
+
+]
+
+
+
+
+
+
+
+
+// const allGoals = JSON.parse(localStorage.getItem('allGoals')) || {
+
+//   first:{
+//     name:'',
+//     completed:false,
+//   },
+//   second:{
+//     name:'',
+//     completed:false,
+//   },
+//   third:{
+//     name:'',
+//     completed:false,
+//   }
+
+// }
+
+const allGoals = JSON.parse(localStorage.getItem('allGoals')) || {}
+
+let completedGoalsCount = Object.values(allGoals).filter((goal) => goal.completed).length
+
+barValue.style.width = (completedGoalsCount / inputboxes.length) * 100 + '%'
+barValue.firstElementChild.innerText = `${completedGoalsCount}/${inputboxes.length} completed`
+quote.innerText = allQuotes[completedGoalsCount]
+footQuote.innerText = footQuotes[completedGoalsCount]
+
 
 
 
@@ -22,19 +69,26 @@ checkboxes.forEach(checkbox => {
 
     if (isInputBoxFilled) {
 
-
-      // var completedTasks = numOfT + 1; 
-      // var widthPercentage = (completedTasks / 3) * 100;
-
-
-      // barValue.style.width = widthPercentage + '%';
-
-      // numberOfTask.innerHTML = completedTasks + '/3 Completed';
-
-      //toggling class Completed
-      
       checkbox.parentElement.classList.toggle("completed")
       error.style.visibility = 'hidden';
+
+      const inputId = checkbox.nextElementSibling.id
+      allGoals[inputId].completed = !allGoals[inputId].completed
+
+      completedGoalsCount = Object.values(allGoals).filter((goal) => goal.completed).length
+
+      barValue.style.width = (completedGoalsCount / inputboxes.length) * 100 + '%'
+      barValue.firstElementChild.innerText = `${completedGoalsCount}/${inputboxes.length} completed`
+
+
+      quote.innerText = allQuotes[completedGoalsCount]
+      footQuote.innerText = footQuotes[completedGoalsCount]
+
+
+
+
+      localStorage.setItem('allGoals', JSON.stringify(allGoals))
+
     }
     else {
       error.style.visibility = 'visible';
@@ -49,8 +103,39 @@ checkboxes.forEach(checkbox => {
 
 inputboxes.forEach((input) => {
 
+  if(allGoals[input.id]){
+    input.value = allGoals[input.id].name;
+
+    if (allGoals[input.id].completed) {
+      input.parentElement.classList.add('completed')
+  
+    }
+  }
+
+
+
   input.addEventListener('focus', () => {
     error.style.visibility = 'hidden';
+  })
+
+
+  input.addEventListener("input", (e) => {
+    if (allGoals[input.id] && allGoals[input.id].completed) {
+      input.value = allGoals[input.id].name
+      return
+    }
+
+    if(allGoals[input.id]){
+      allGoals[input.id].name= input.value
+    }
+    else{
+      allGoals[input.id]={
+        name:'',
+        completed:false
+      }
+    }
+
+    localStorage.setItem('allGoals', JSON.stringify(allGoals))
   })
 
 })
